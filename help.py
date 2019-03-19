5c1eba7617b354a4cf64cdbf23f1299bb677b4ef
Gabriel-06 <44278698+Gabriel-06@users.noreply.github.com>
2019-03-20 00:26:27 +1030

    WIP

--- help.py
+++ help.py
@@ -1,7 +1,24 @@
-import PyNUT
+def GetUPSVars(self, ups=""):
+    """ Get all available vars from the specified UPS
+The result is a dictionary containing 'key->val' pairs of all
+available vars.
+    """
+    if self.__debug:
+        print("[DEBUG] GetUPSVars called...")
 
-nut_handler = PyNUT.PyNUTClient(host=10.0.0.205, port=port, login=login, password=password)
-upses = nut_handler.GetUPSList()
+    self.__srv_handler.write("LIST VAR %s\n" % ups)
+    result = self.__srv_handler.read_until("\n")
+    if result != "BEGIN LIST VAR %s\n" % ups:
+        raise PyNUTError(result.replace("\n", ""))
 
-ups_list = upses.keys()
-ups_list.sort()
+    ups_vars = {}
+    result = self.__srv_handler.read_until("END LIST VAR %s\n" % ups)
+    offset = len("VAR %s " % ups)
+    end_offset = 0 - (len("END LIST VAR %s\n" % ups) + 1)
+
+    for current in result[:end_offset].split("\n"):
+        var = current[offset:].split('"')[0].replace(" ", "")
+        data = current[offset:].split('"')[1]
+        ups_vars[var] = data
+
+    return (ups_vars)
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import PyNUT
 import time
 
+originalTime=0
+state=0
+
 def cleanString(inText):
     inText = inText.rstrip()
     params, inText = inText.split("=",1)
     inText = str(inText)
     return inText
+    
 
 
 dataFile = open("NUT.conf", "r")
@@ -14,18 +18,31 @@ password = cleanString(dataFile.readline())
 host = cleanString(dataFile.readline())
 port = cleanString(dataFile.readline())
 upsName = cleanString(dataFile.readline())
+shutdownTime = cleanString(dataFile.readline())
+
+shutdownTime = shutdownTime*60,000
 
 print(login)
 print(password)
 print(host)
 print(port)
 print(upsName)
+print(shutdownTime)
 
 ups_handler=PyNUT.PyNUTClient(host=host, port=port, login=login, password=password)
-upsInfo = ups_handler.GetUPSVars("qnapups")
-
-print(upsInfo)
+upsInfo = ups_handler.GetUPSVars(upsName)
 
 while True:
-    upsInfo = ups_handler.GetUPSVars("qnapups")
-    time.sleep(1000)
+    upsInfo = ups_handler.GetUPSVars(upsName)
+
+    if upsInfo['ups.status']=='OB DISCHRG' and state!=1:
+        originalTime = time.time()
+        state=1
+        
+    if upsInfo['ups.status']=='OB DISCHRG' :
+        if(int(originalTime)+int(  shutdownTime))==time.time():
+            #TODO shutdown computer
+        print("PANIC")
+    if upsInfo['ups.status']=='OL':
+        state=0
+
