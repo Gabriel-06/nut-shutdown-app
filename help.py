d32af3e0a1418947cd85d3de71f2d3126a7b3390
Gabriel-06 <44278698+Gabriel-06@users.noreply.github.com>
2019-04-25 15:22:30 +0930

    Added shutdown functionality

--- help.py
+++ help.py
@@ -1,24 +1,3 @@
-def GetUPSVars(self, ups=""):
-    """ Get all available vars from the specified UPS
-The result is a dictionary containing 'key->val' pairs of all
-available vars.
-    """
-    if self.__debug:
-        print("[DEBUG] GetUPSVars called...")
+import os
 
-    self.__srv_handler.write("LIST VAR %s\n" % ups)
-    result = self.__srv_handler.read_until("\n")
-    if result != "BEGIN LIST VAR %s\n" % ups:
-        raise PyNUTError(result.replace("\n", ""))
-
-    ups_vars = {}
-    result = self.__srv_handler.read_until("END LIST VAR %s\n" % ups)
-    offset = len("VAR %s " % ups)
-    end_offset = 0 - (len("END LIST VAR %s\n" % ups) + 1)
-
-    for current in result[:end_offset].split("\n"):
-        var = current[offset:].split('"')[0].replace(" ", "")
-        data = current[offset:].split('"')[1]
-        ups_vars[var] = data
-
-    return (ups_vars)
+os.system("open quitall.app")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PyNUT
 import time
+import os
 
 originalTime=0
 state=0
@@ -9,7 +10,11 @@ def cleanString(inText):
     params, inText = inText.split("=",1)
     inText = str(inText)
     return inText
-    
+
+def notify(title, text, sound):
+    os.system("""osascript -e 'display notification "{}" with title "{}" sound name "{}"'
+              """.format(text, title, sound))
+
 
 
 dataFile = open("NUT.conf", "r")
@@ -20,29 +25,33 @@ port = cleanString(dataFile.readline())
 upsName = cleanString(dataFile.readline())
 shutdownTime = cleanString(dataFile.readline())
 
-shutdownTime = shutdownTime*60,000
+shutdownTimeMS = shutdownTime*60,000
 
 print(login)
 print(password)
 print(host)
 print(port)
 print(upsName)
-print(shutdownTime)
+print(shutdownTimeMS)
 
 ups_handler=PyNUT.PyNUTClient(host=host, port=port, login=login, password=password)
 upsInfo = ups_handler.GetUPSVars(upsName)
 
 while True:
     upsInfo = ups_handler.GetUPSVars(upsName)
+    print(upsInfo['ups.status'])
 
     if upsInfo['ups.status']=='OB DISCHRG' and state!=1:
+        print("PANIC")
         originalTime = time.time()
         state=1
-        
+
     if upsInfo['ups.status']=='OB DISCHRG' :
-        if(int(originalTime)+int(  shutdownTime))==time.time():
-            #TODO shutdown computer
-        print("PANIC")
+        if(int(originalTime)+int(shutdownTimeMS))==time.time():
+            notify("UPS Alert", "Detected UPS power loss. Shutting down computer in "+shutdownTime+" minute(s).", "Funk")
+            os.system("open quitall.app")
+
     if upsInfo['ups.status']=='OL':
         state=0
 
+    time.sleep(10)
